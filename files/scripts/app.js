2f77d4bd87310ca6b557bf65fd8d47bee357a70d
mersad <mersad.up@gmail.com>
2023-09-24 20:39:39 +0330

    remove popup search bar

--- files/scripts/app.js
+++ files/scripts/app.js
@@ -5,9 +5,7 @@ let allCollectionPopupMenu = $.querySelector(".all-collections__popup-list");
 let allCollectionMenuBtn = $.querySelector(".nav-bar__all-collections");
 let mobileMenu = $.querySelector(".mobile-menu");
 let searchBtn = $.querySelector(".search-bar__btn");
-let mobileSearchBar = $.querySelector(".mobile-search-bar");
 let mobileMenuCover = $.querySelector(".cover");
-let searchBarInput = $.querySelector(".search-bar__input");
 let menuActive = false;
 let searchBarActive = false;
 let windowSize = window.innerWidth;
@@ -44,19 +42,6 @@ allCollectionMenuBtn.addEventListener("click", () => {
   allCollectionPopupMenu.classList.toggle("all-collections__popup-list--open");
   openMenu();
 });
-// open search Bar
-searchBtn.addEventListener("click", openSearchBar);
-searchBarInput.addEventListener("focus" , ()=>{
-  console.log(searchBarActive);
-  if(searchBarActive == true){
-    return
-  }
-  else{
-    openSearchBar();
-  }
-})
-//close search Bar
-mobileSearchBar.addEventListener("click", closeSearchBar , false);
 
 //
 // Functions
@@ -64,6 +49,7 @@ mobileSearchBar.addEventListener("click", closeSearchBar , false);
 
 //open mobile menu
 function openMenu() {
+  console.log("open mobile menu");
   mobileMenu.classList.add("mobile-menu--open");
   mobileMenuCover.classList.add("cover--active");
   if (windowSize <= 992) {
@@ -72,25 +58,11 @@ function openMenu() {
 }
 //close mobile menu
 function closeMenu() {
+  console.log("close mobile menu");
   mobileMenu.classList.remove("mobile-menu--open");
   mobileMenuCover.classList.remove("cover--active");
   HtmlElem.style.scrollBehavior = "smooth";
   menuActive = false;
-}
-// open mobile search bar
-function openSearchBar() {
-  let searchBarInput = $.querySelector(".mobile-search-bar .search-bar__input");
-  searchBarInput.focus();
-  mobileSearchBar.classList.add("mobile-search-bar--open");
-  HtmlElem.style.scrollBehavior = "unset";
-  searchBarActive = true;
-}
-// close mobile search bar
-function closeSearchBar() {
-  console.log('close search bar with mobileSearchBar');
-  mobileSearchBar.classList.remove("mobile-search-bar--open");
-  HtmlElem.style.scrollBehavior = "smooth";
-  searchBarActive = false;
 }
 
 //
@@ -107,3 +79,21 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: false,
   },
 });
+
+//
+// Helper functions
+//
+
+// add class function
+function _addRemoveClass(action, element, classes) {
+  if (action == "add") {
+    element.classList.add(`${classes}`);
+  } else if (action == "remove") {
+    element.classList.remove(`${classes}`);
+  } else {
+    console.log(
+      "action not true",
+      ` element:${element} classes:${classes} action:${action}`
+    );
+  }
+}
